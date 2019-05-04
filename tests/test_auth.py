d86afde3d03f4229fda2034089a5b7d459a749fb
Andrew Woodill <johnwoodill@gmail.com>
2019-05-04 15:12:45 -0700

    updating testing

---
+++ tests/test_auth.py
@@ -0,0 +1,15 @@
+# Set folder parent directory to check modules
+import sys
+sys.path.insert(0,'..')
+import spatialIUU.processGFW as siuu
+import pytest
+
+def test_spherical_dist_populate():
+    
+    # Paris to Amsterdam in km
+    # Paris lat = 48.8566 lon = 2.3522
+    # Amsterdam lat = 52.3680 lon = 4.9036
+    # Distance 430km
+    #gfwD = dataStep
+    dist = round(siuu.dataStep.spherical_dist_populate([48.8566, 52.3680], [2.3522, 4.9036])[1][0], 0)
+    assert (dist == 430)
