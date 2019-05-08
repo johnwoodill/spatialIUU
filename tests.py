1b71f36b5979769411746aa73de89cd365f0f260
Andrew Woodill <johnwoodill@gmail.com>
2019-05-08 11:19:49 -0700

    added distance matrix module

--- tests.py
+++ tests.py
@@ -2,8 +2,10 @@ import os, pathlib
 import pytest
 
 
-test = [f"{pathlib.Path.cwd()}/tests/test-processGFW.py"]
-pytest.main(test)
+test1 = [f"{pathlib.Path.cwd()}/tests/test-processGFW.py"]
+test2 = [f"{pathlib.Path.cwd()}/tests/test-distanceMatrix.py"]
+pytest.main(test1)
+pytest.main(test2)
 
 
 
